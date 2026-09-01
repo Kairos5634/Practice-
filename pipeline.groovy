2bce00bc0f2d51f2127cd28f7a873168cfba511e
Aryan <aryanpingale2004@gmail.com>
2026-09-01 12:06:30 +0530

    fixed

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,30 +3,32 @@ pipeline {
     stages {
         stage ('PULL') {
             steps {
-                git branch: 'main', url: 'https://github.com/jambhulkarcloudblitz-alt/CDEC-studentapp.git' 
-            }
-        }
-        stage ('BUILD') {
-            steps {
-                sh '''cd backend
-                        mvn clean package -DskipTests
-                        '''
-            }
-        }
-        stage ('TEST') {
-            steps {
-                sh '''cd backend
-                        mvn clean verify sonar:sonar \
-                         -Dsonar.projectKey=pipeline \
-                          -Dsonar.projectName='pipeline' \
-                          -Dsonar.host.url=http://51.20.193.186:9000 \
-                          -Dsonar.token=sqp_d8ea28575d2879e33f33188fea6a9022df4bd88d
-
-                        '''
+                git branch: 'main', url: 'https://github.com/Kairos5634/Student-app.git'
             }
         }
     }
 }
+        // stage ('BUILD') {
+        //     steps {
+        //         sh '''cd backend
+        //                 mvn clean package -DskipTests
+        //                 '''
+        //     }
+        // }
+//         stage ('TEST') {
+//             steps {
+//                 sh '''cd backend
+//                         mvn clean verify sonar:sonar \
+//                          -Dsonar.projectKey=pipeline \
+//                           -Dsonar.projectName='pipeline' \
+//                           -Dsonar.host.url=http://51.20.193.186:9000 \
+//                           -Dsonar.token=sqp_d8ea28575d2879e33f33188fea6a9022df4bd88d
+
+//                         '''
+//             }
+//         }
+//     }
+// }
 //         stage ('TEST'){
 //             steps {
 //                 sh 'echo "TEST SUCCESS"'
@@ -39,3 +41,13 @@ pipeline {
 //         }
 //     }
 // }
+
+
+
+// sqp_95ad8eb2dae20ecd08fbd3a8b78e160707781467
+
+// mvn clean verify sonar:sonar \
+//   -Dsonar.projectKey=student-app \
+//   -Dsonar.projectName='student=app' \
+//   -Dsonar.host.url=http://16.171.206.249:9000 \
+//   -Dsonar.token=sqp_95ad8eb2dae20ecd08fbd3a8b78e160707781467
